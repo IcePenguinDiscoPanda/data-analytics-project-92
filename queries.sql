d51d960c7f914b530ab513d5f2328c331e2a3056
IcePenguinDiscoPanda <31739979+IcePenguinDiscoPanda@users.noreply.github.com>
2023-07-24 20:30:40 +0300

    added days_of_the_week_jncome + query update

--- queries.sql
+++ queries.sql
@@ -1,10 +1,12 @@
 --запрос, который считает общее количество покупателей
+
 select
     count(customer_id) as customers_count 
 from customers c;
 
 
 --топ 10 продавцов с наибольшей выручкой
+
 select 
     concat(first_name, ' ',  last_name) as name, 
     count (s.sales_id) operations, 
@@ -16,6 +18,7 @@ join products p using (product_id)
 group by concat(first_name, ' ',  last_name)
 order by income desc
 limit 10;
+
 
 --продавцы со средней выручкой меньше общей средней
 
@@ -40,3 +43,25 @@ from interim_table
 where average_income < (select total_avg_income 
 			from total_avg_amount)
 order by 2 asc;
+
+
+--данные по выручке по каждому продавцу по дню недели
+
+with sorted_by_day_id as (
+select 
+	concat(first_name, ' ',  last_name) as name, 
+	to_char(sale_date, 'DAY') as weekday,
+	to_char(sale_date, 'ID') as weekday_id,
+	round(sum(p.price * s.quantity),0) as income
+from employees e 
+left join sales s 
+on e.employee_id = s.sales_person_id 
+join products p using (product_id)
+group by concat(first_name, ' ',  last_name), to_char(sale_date, 'DAY'), to_char(sale_date, 'ID')
+order by weekday_id, name
+)
+select 
+	name, 
+	weekday, 
+	income 
+from sorted_by_day_id;
